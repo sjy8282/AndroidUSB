3ff593b7074cedec1d577eb9eaae9605f9f4ad75
Jeyong Shin <sjy8282@hotmail.com>
2011-08-26 11:47:08 +0900

    add AndroidUSB source

---
+++ src/com/jeyong/usb/MyActivity.java
@@ -0,0 +1,11 @@
+
+package com.jeyong.usb;
+
+import android.app.Activity;
+
+
+class MyActivity extends Activity{
+	protected void onCreate(android.os.Bundle savedInstanceState) {
+		setContentView(R.layout.main);
+	}
+}
